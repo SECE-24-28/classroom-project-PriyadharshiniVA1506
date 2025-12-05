05beb2d060e4f516843c46bb88d2b7c4864b4e57
PriyadharshiniVA1506 <vapriyadharshini06@gmail.com>
2025-12-05 08:53:03 +0530

    Initial commit

---
+++ react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import RectangleBox from "./rectangle-box/rectangle-box-component";
+import Mycomponent from "./Galance/galance-component";
+import ContactSection from "./Contact/ContactSection";
+function App() {
+  return (
+    <>
+      <RectangleBox />
+      <Mycomponent/>
+       <ContactSection />
+    
+    </>
+  );
+}
+
+export default App;
---
+++ react-app/src/leaders-tomorrow/leaders-tomorrow-component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { LeadersTomorrowStyle } from "./leaders-tomorrow-style";
+const LeadersTomorrowComponent = () => {
+  return (
+    <LeadersTomorrowStyle>
+      <div className="capsule">
+        <h1 className="heading">Learners Today, Leaders Tomorrow</h1>
+        <p className="sub__heading">
+          the essential skills and knowledge to excel in your professional endeavors. Our expert training in aptitude and coding equips you to conquer every challenge, from company interview assessments to prestigious coding platforms like LeetCode, GeeksForGeeks, HackerRank , and more.
+        </p>
+      </div>
+      <div className="content-section">
+        <h2 className="section-heading">A Symbol of Excellence in Placement Training</h2>
+        <p className="section-text">
+          Aptitude Guru Hem was born from the aspiration to create a learning environment that inspires greatness. Our founder, driven by personal experiences and a desire to make a difference, established the company with a mission to empower students with the right skills and mindset for success.
+        </p>
+        <h2 className="section-heading">Our Vision</h2>
+        <p className="section-text">
+          Our vision is to empower individuals from diverse backgrounds to become accomplished professionals. At Aptitude Guru Hem, we believe that knowledge has the power to transcend boundaries.
+        </p>
+        <h2 className="section-heading">Our Mission</h2>
+        <p className="section-text">
+          Our mission is to revolutionize education by providing comprehensive training in aptitude, coding, and web/mobile development. We foster a culture of inclusivity and personal growth.
+        </p>
+      </div>
+    </LeadersTomorrowStyle>
+  );
+};
+export default LeadersTomorrowComponent;
