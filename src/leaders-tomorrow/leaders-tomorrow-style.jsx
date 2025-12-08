b609ea9513524c2ff2df67ac50e3b72dfa69f1a2
PriyadharshiniVA1506 <vapriyadharshini06@gmail.com>
2025-12-08 08:48:12 +0530

    Adding files

---
+++ src/leaders-tomorrow/leaders-tomorrow-style.jsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+export const LeadersTomorrowStyle = styled.div`
+  .capsule {
+    background-color: #ff3c00ff;
+    border-radius: 200px;
+    max-width: 1800px;
+    padding: 4rem 6rem;
+  }
+  .heading {
+    color: white;
+    font-size: 32px;
+    color: white;
+    font-weight: 600;
+    text-align: center;
+  }
+  .sub__heading {
+    color: white;
+    font-weight: 500;
+    font-size: 20px;
+    text-align: center;
+  }
+  .content-section {
+    margin: 2rem 0;
+    padding: 2rem;
+  }
+  .section-heading {
+    color: #000000 !important;
+    font-size: 24px;
+    font-weight: 600;
+    margin-bottom: 1rem;
+  }
+  .section-text {
+    color: #000000 !important;
+    font-size: 16px;
+    line-height: 1.6;
+    margin-bottom: 1.5rem;
+  }
+  p, h1, h2, h3, div {
+    color: #000000 !important;
+  }
+`;
