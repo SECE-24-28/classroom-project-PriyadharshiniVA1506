d3483e7658a473b2fa26907ca13a815d2533e315
PriyadharshiniVA1506 <vapriyadharshini06@gmail.com>
2025-12-12 09:23:03 +0530

    Adding files

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,39 @@
-/*import "./App.css";
-import LeadersTomorrowComponent from "./leaders-tomorrow/leaders-tomorrow-component";
-
-function App() {
-  return (
-    <div>
-      {<HelloWorldComponent /> }
-      {<FlexBoxComponent /> }
-      <LeadersTomorrowComponent />
-      
-    </div>
-  );
-}
-
-export default App;
-
-*/
-
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import RectangleBox from "./rectangle-box/rectangle-box-component";
+import Mycomponent from "./Galance/galance-component";
+import ContactSection from "./Contact/ContactSection";
+import OurPromisesComponent from "./our-promises/our-promises";
+import TrustedbyComponent from "./trusted-by/trusted-by-component";
+// import College from "./college/college-component.jsx";
+import NotFound from "./NotFound";
 import FetchApiComponent from "./fetch-api/fetch-api-component";
-import UseEffectComponent from "./use-effect-practice/use-effect-practice";
 import UseEffectPracticeTwo from "./use-effect-practice/use-effect-practice-2";
-import UseEffectPracticeThree from "./use-effect-practice/use-effect-practice-3";
+import { Routes, Route } from "react-router-dom";
+//import UseStateComponent  from "./usestate/use-state-component.jsx";
+import AdditionOperationComponent   from "./addition-operation-in-state/addition-operation-component.jsx";
+
+
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<FetchApiComponent />} />
+      {/* Main sections */}
+      <Route path="/" element={<RectangleBox />} />
+      <Route path="/galance" element={<Mycomponent />} />
+      <Route path="/trusted-by" element={<TrustedbyComponent />} />
+      <Route path="/our-promises" element={<OurPromisesComponent />} />
+      <Route path="/contact" element={<ContactSection />} />
+      {/* <Route path="/college" element={<College />} /> */}
+
+      {/* Practice pages */}
       <Route path="/api-fetch" element={<FetchApiComponent />} />
-      <Route path="/use-effect-practice" element={<UseEffectComponent />} />
-        <Route
-          path="/use-effect-practice-2"
-          element={<UseEffectPracticeTwo />}
-        />
-         <Route
-          path="/use-effect-practice-3"
-          element={<UseEffectPracticeThree />}
-        />
-      </Routes>
+      <Route path="/use-effect-practice-2" element={<UseEffectPracticeTwo />} />
+      <Route path="/addition-operation" element={<AdditionOperationComponent />} />
+
       
-   
-     
+      {/* 404 Page */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
